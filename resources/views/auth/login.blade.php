32e39660f7a33faeba0ab8379dc9e266d5834cfc
fjuncal <fabriciojuncal@micam.com.ar>
2022-09-17 17:22:44 -0300

    180. Modificando el Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,17 +41,24 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+            <div class="flex justify-between mt-5 mb-4">
+                <x-link
+                    :href="route('register')"
+                >
+                    Crear Cuenta
+                </x-link>
 
-                <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-primary-button>
+                <x-link
+                    :href="route('password.request')"
+                >
+                    Olvidaste tu Password
+                </x-link>
+
             </div>
+
+            <x-primary-button class="ml-3">
+                {{ __('Log in') }}
+            </x-primary-button>
         </form>
     </x-auth-card>
 </x-guest-layout>
