8e802b45b4b864611eeeedbfb4d2fdcfd23ed223
fjuncal <fabriciojuncal@micam.com.ar>
2022-09-21 22:36:18 -0300

    186. Personalizar el Email que se envia

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,5 +29,20 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        VerifyEmail::toMailUsing(function($notifiable, $url){
+            return (new MailMessage)
+                // Titulo
+                ->subject('Verificar Cuenta')
+                // Saludo del Inicio
+                ->greeting('¡Hola!')
+				// Linea de Texto
+                ->line('Tu Cuenta ya esta casi lista, solo debes presionar el enlace a continuación')
+                // Botón de redirección
+                ->action('Confirmar Cuenta', $url)
+                // Linea de Texto
+                ->line('Si no creaste esta cuenta, puedes ignorar este mensaje')
+                // Despedida
+				->salutation('¡Chau!');
+        });
     }
 }
