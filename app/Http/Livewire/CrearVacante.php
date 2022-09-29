313f06b40f3487a3df26034ae8ff0f3612a76014
fjuncal <fabriciojuncal@micam.com.ar>
2022-09-28 22:09:30 -0300

    197. Conectar el Formulario a Livewire

--- app/Http/Livewire/CrearVacante.php
+++ app/Http/Livewire/CrearVacante.php
@@ -8,6 +8,29 @@ use Livewire\Component;
 // Los PHP de los componentes tienen la funcionalidad similar a la de un Controlador de Laravel
 class CrearVacante extends Component
 {
+
+    /** Creamos los atributos que se conectaran con los campos del Frontend mediante Livewiere **/
+
+    public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion;
+    public $imagen;
+
+    /** Agregamos las reglas de validación para los campos del formulario **/
+    protected $rules = [
+        'titulo' => 'required|string',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required',
+        'imagen' => 'required',
+    ];
+
+
     public function render()
     {
         // Consultar BD
--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -2,15 +2,15 @@
     <!-- Titulo Vacante -->
     <div>
         <x-input-label for="titulo" :value="__('Titulo Vacante')" />
-
-        <x-text-input id="titulo" class="block mt-1 w-full" type="text" name="titulo" :value="old('titulo')" placeholder="Titulo Vacante" />
+        {{-- wire:model => Suplanta al atributo "name" y sirve para sincronizar con los atributos directamente del backend mediante Livewire --}}
+        <x-text-input id="titulo" class="block mt-1 w-full" type="text" wire:model="titulo" :value="old('titulo')" placeholder="Titulo Vacante" />
     </div>
 
     <!-- Salario Mensual -->
     <div>
         <x-input-label for="salario" :value="__('Salario Mensual')" />
-
-        <select name="salario" id="salario" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
+        {{-- wire:model => Suplanta al atributo "name" y sirve para sincronizar con los atributos directamente del backend mediante Livewire --}}
+        <select wire:model="salario" id="salario" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
 
             <option>-- Seleccione --</option>
             @foreach ($salarios as $salario)
@@ -22,8 +22,8 @@
     <!-- Categoría -->
     <div>
         <x-input-label for="categoria" :value="__('Categoría')" />
-
-        <select name="categoria" id="categoria" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
+        {{-- wire:model => Suplanta al atributo "name" y sirve para sincronizar con los atributos directamente del backend mediante Livewire --}}
+        <select wire:model="categoria" id="categoria" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
             <option>-- Seleccione --</option>
             @foreach ($categorias as $categoria)
                 <option value="{{ $categoria->id }}">{{ $categoria->categoria }}</option>
@@ -34,30 +34,30 @@
     <!-- Empresa -->
     <div>
         <x-input-label for="empresa" :value="__('Empresa')" />
-
-        <x-text-input id="empresa" class="block mt-1 w-full" type="text" name="empresa" :value="old('empresa')" placeholder="Empresa: ej. Netflix, Uber, Shopify"/>
+        {{-- wire:model => Suplanta al atributo "name" y sirve para sincronizar con los atributos directamente del backend mediante Livewire --}}
+        <x-text-input id="empresa" class="block mt-1 w-full" type="text" wire:model="empresa" :value="old('empresa')" placeholder="Empresa: ej. Netflix, Uber, Shopify"/>
     </div>
 
     <!-- Último Día para postularse -->
     <div>
         <x-input-label for="ultimo_dia" :value="__('Último Día para postularse')" />
-
-        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" name="ultimo_dia" :value="old('ultimo_dia')"/>
+        {{-- wire:model => Suplanta al atributo "name" y sirve para sincronizar con los atributos directamente del backend mediante Livewire --}}
+        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" wire:model="ultimo_dia" :value="old('ultimo_dia')"/>
     </div>
 
     <!-- Último Día para postularse -->
     <div>
         <x-input-label for="descripcion" :value="__('Descripción Puesto')" />
-
-        <textarea id="descripcion" name="descripcion" placeholder="Descripción general del puesto, experiencia"  class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-72" />
+        {{-- wire:model => Suplanta al atributo "name" y sirve para sincronizar con los atributos directamente del backend mediante Livewire --}}
+        <textarea id="descripcion" wire:model="descripcion" placeholder="Descripción general del puesto, experiencia"  class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-72" />
         </textarea>
     </div>
 
     <!-- Imagen -->
     <div>
         <x-input-label for="imagen" :value="__('imagen')" />
-
-        <x-text-input id="imagen" class="block mt-1" type="file" name="imagen"/>
+        {{-- wire:model => Suplanta al atributo "name" y sirve para sincronizar con los atributos directamente del backend mediante Livewire --}}
+        <x-text-input id="imagen" class="block mt-1" type="file" wire:model="imagen"/>
     </div>
 
     {{-- Se pueden agregar clases a los componentenes en donde estos sobreescribiran los estilos por defectos del componente --}}
