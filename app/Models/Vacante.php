4227170f22a588657df8f21f3c7175a622402efc
fjuncal <fabriciojuncal@micam.com.ar>
2022-10-13 21:42:24 -0300

    210. Creando botones para administración

--- app/Models/Vacante.php
+++ app/Models/Vacante.php
@@ -9,6 +9,10 @@ class Vacante extends Model
 {
     use HasFactory;
 
+    // De esta manera definimos que el campo "ultimo_dia" tendrá el formato "Fecha"
+    // Ya que si no lo definimos se obtendra un error al querer formatear la fecha con los helpers de Laravel
+    protected $dates = ['ultimo_dia'];
+
     protected $fillable = [
         'titulo',
         'salario_id',
