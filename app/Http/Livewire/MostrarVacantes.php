9d330b2b0f44fdff1f6e4356d80194d1cbd3952c
fjuncal <fabriciojuncal@micam.com.ar>
2022-10-26 21:03:51 -0300

    223. Emitir eventos desde la vista hasta el Componente y eliminar

--- app/Http/Livewire/MostrarVacantes.php
+++ app/Http/Livewire/MostrarVacantes.php
@@ -9,14 +9,29 @@ class MostrarVacantes extends Component
 {
 
     // public  = ['prueba1', 'prueba2', 'etc..] => Este array debe ser escrito tal cual esta para que funcione. Así "$listeners"
-    // Lo que hace este array es almacenar todos los métodos habilitados para ser ejecutados por eventos (en este caso "wire:click" que ejecuta el méotodo "prueba")
+    // Lo que hace este array es almacenar todos los métodos habilitados que pueden ser ejecutados por eventos:
+    // Como por ejemplo:
+
+    //  "wire:click="$emit('mostrarAlerta', {{ $vacante->id }}) => En este caso ejecutamos el método "mostrarAlerta" desde JS al hacer click
     // en el archivo "resources\views\livewire\mostrar-vacantes.blade.php" Linea 33
 
-    public $listeners = ['prueba'];
+    // O tambien un método en PHP puede ser ejecutado con:
 
-    public function prueba($vacante_id)
+    // "Livewire.emit('eliminarVacante')" => De esta manera ejecutamos el método "eliminarVacante" desde JS sin necesidad de esperar a que se ejecute un método
+    // en el archivo "resources\views\livewire\mostrar-vacantes.blade.php" Linea 67
+
+    public $listeners = ['eliminarVacante'];
+
+
+    // (Vacante $vacante) => De esta manera instanciamos el modelo de manera automatica y gracias al Route Model Binding de Laravel y obtendremos toda los datos de la vacante.
+    //                      Es decir, desde el Front enviamos "eliminarVacante([ID VACANTE])" y al instanciar el modelo y tener el ID de la Vacante, por detras se ejecuta una
+    //                      Query que sería algo como "SELECT * FROM Vacante WHERE id = [ID VACANTE]"
+    public function eliminarVacante(Vacante $vacante)
     {
-        dd('Desde el Código de PHP => ID nro: ' . $vacante_id);
+        // Gracias al ORM de Laravel llamado Eloquent podremos eliminar un registro de la siguiente manera, donde:
+        // $vacante => Es una instancia del Modelo "Vacante" y se tiene el registro de ese ID
+        // ->delete() => Es el equivalente de "DELETE INTO Vacante WHERE id = [ID VACANTE] (Al tener la instancia del modelo ya se obtiene el ID de manera automatica gracias al Route Model Binding de Laravel)
+        $vacante->delete();
     }
 
     public function render()
--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -30,7 +30,7 @@
                             1er Parametro => El 1er parametro que recibe es el nombre de la función definido en el controlador del componente (en el PHP) o en el apartado de scripts (en el JS) --}}
                     {{--    2do Parametro => Como 2do parametro recibe una variable con valores para procesarlos desde el contolador del componente (en el PHP) o en el apartado de scripts (en el JS) --}}
                     <button
-                        wire:click="$emit('prueba', {{ $vacante->id }})"
+                        wire:click="$emit('mostrarAlerta', {{ $vacante->id }})"
                         class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
                         Eliminar
                     </button>
@@ -47,32 +47,49 @@
 
 </div>
 
+{{--
+    @push('scripts') => Directiva que agrega todo el contenido en el espacio que se reservó con la directiva  @stack('scripts').
+        Parametro => Acepta como parametro el nombre del espacio reservado.
+        Por ejemplo, en este caso se reservó agregando "@stack('scripts')", por lo tanto hacemos referencia a ese espeacio reservado con "@push('scripts')"
+--}}
 @push('scripts')
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
 
-        Livewire.on('prueba', vacanteId => {
-            alert('Desde el Código de JS => ID nro: ' + vacanteId);
+        // Livewire.on () => Permite Escuchar eventos en JavaScript, es decir, en este caso estará escuchando
+        // si se ejecuta el evento "Click" (que se agregó con "wire:click="$emit('mostrarAlerta', {{ $vacante->id }})"").
+        // Si se ejecuta el evento, mostrará la Alerta.
+        // De esta manera podemos ejecutar una acción en el Front luego de que se ejecute un evento.
+        //      1er Parametro => Nombre de la función, es decir en este caso se agregó "$emit('mostrarAlerta', {{ $vacante->id }})", por lo tanto el evento "Click"
+        //                      Ejecutará la función "mostrarAlerta" en donde le pasamos el ID de la Vacante como parametro
+        //      2do Parametro => Función (o callback) que se ejecutará al cumplirse el evento
+        Livewire.on('mostrarAlerta', vacanteId => {
+            Swal.fire({
+            title: '¿Eliminar Vacante?',
+            text: "Una vacante eliminado no se puede recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, ¡Eliminar!',
+            cancelButtonText: 'Cancelar'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                // Eliminar la vacante
+                // Ejecuta el método "eliminarVacante()" en el PHP del componente
+                // De esta manera desde JS ejecutamos una función en PHP de una manera facil
+                Livewire.emit('eliminarVacante', vacanteId);
+
+                Swal.fire(
+                '¡Vacante Eliminada!',
+                'Se eliminó la vacante correctamente',
+                'success'
+                )
+            }
+            });
         });
 
-        // Swal.fire({
-        // title: '¿Eliminar Vacante?',
-        // text: "Una vacante eliminado no se puede recuperar",
-        // icon: 'warning',
-        // showCancelButton: true,
-        // confirmButtonColor: '#3085d6',
-        // cancelButtonColor: '#d33',
-        // confirmButtonText: 'Si, ¡Eliminar!',
-        // cancelButtonText: 'Cancelar'
-        // }).then((result) => {
-        // if (result.isConfirmed) {
-        //     Swal.fire(
-        //     '¡Vacante Eliminada!',
-        //     'Se eliminó la vacante',
-        //     'success'
-        //     )
-        // }
-        // })
+
     </script>
 @endpush
