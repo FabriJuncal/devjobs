b9bcb9ce8f0b695d0a709cbbc69908c75d964838
fjuncal <fabriciojuncal@micam.com.ar>
2022-11-02 22:07:43 -0300

    235. Validación del Formulario

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -6,6 +6,25 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+
+    public $cv;
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    public function postularme(){
+
+        // Valida el formulario de postulación
+        $datos = $this->validate();
+
+        // Crea la vacante
+
+        // Crea la notificación y envía un email
+
+        // Muestra al usuario un mensaje de ok
+
+    }
+
     public function render()
     {
         return view('livewire.postular-vacante');
--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,12 +1,16 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
     <h3 class="text-center text-2xl font-bold my-4">Postularme a esta vacante</h3>
 
-    <form class="w-96 mt-5">
+    <form wire:submit.prevent="postularme" class="w-96 mt-5">
         <div class="mb-4">
             <x-input-label for="cv" :value="__('Curriculum u Hoja de Vida (PDF)')" />
             {{-- accept=".pdf" => Indicamos que solo se podrá subír archivos de tipo ".pdf" --}}
             <x-text-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full"/>
         </div>
+
+        @error('cv')
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
 
         {{-- Se pueden agregar clases a los componentenes en donde estos sobreescribiran los estilos por defectos del componente --}}
         <x-primary-button class="my-5">
