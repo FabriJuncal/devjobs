5c6a51cbcb16a4fd2c92f6e682b1592c7b3f227d
fjuncal <fabriciojuncal@micam.com.ar>
2022-11-07 20:41:27 -0300

    240. Creando la Notificación y ejecutando migración

---
+++ app/Notifications/NuevoCandidato.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NuevoCandidato extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Obtener los canales de entrega de la notificación.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Obtener la representación por correo de la notificación.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+
+    //  Método que viene ya definido en Laravel y se utiliza para el envio de Mails
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    public function toDataBase($notifiable)
+    {
+
+    }
+
+    /**
+     * Obtener la representación de matriz de la notificación.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
