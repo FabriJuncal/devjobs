7836b578ea59f0c08cd6732ed348b5fcfffd7607
fjuncal <fabriciojuncal@micam.com.ar>
2022-11-14 23:37:31 -0300

    248. Ocultando Notificaciones para Devs y Evitando que accedan a notificaciones

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,16 +10,26 @@
                     </a>
                 </div>
                 @auth
-                    <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        {{-- request()->routeIs => Resalta la opción del menú con Estilos CSS si la ruta actual es igual a valor pasado como parametro --}}
-                        <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
-                            {{ __('Mis Vacantes') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
-                            {{ __('Crear Vacante') }}
-                        </x-nav-link>
-                    </div>
+                    {{-- Se muestra si el usuario cumple con la lógica del Policy de la ruta: --}}
+                    {{-- Linea: 54 --}}
+                    {{-- Archivo: app\Policies\VacantePolicy.php --}}
+
+                    {{-- @can('[NOMBRE DEL MÉTODO DEL POLICY]', [RUTA DEL MODELO]) => Directiva de Laravel que ejecuta el código dentro solos si el la condición del Policy se cumple.
+                                                                                        Tambien existe la directiva opesta a esta, que es "@cannot()"--}}
+                    {{-- 1er Parametro => Nombre del método del Policy --}}
+                    {{-- 2do Parametro => Ruta del Modelo (De otra forma no funcionará) --}}
+                    @can('create', App\Models\Vacante::class)
+                        <!-- Navigation Links -->
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            {{-- request()->routeIs => Resalta la opción del menú con Estilos CSS si la ruta actual es igual a valor pasado como parametro --}}
+                            <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
+                                {{ __('Mis Vacantes') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
+                                {{ __('Crear Vacante') }}
+                            </x-nav-link>
+                        </div>
+                    @endcan
                 @endauth
 
             </div>
@@ -28,14 +38,20 @@
                 <!-- Settings Dropdown -->
                 <div class="hidden sm:flex sm:items-center sm:ml-6">
                     @auth
-                    {{-- Mostramos la Cantidad de Notificaciones sin ver --}}
-                    {{-- Validamos que solo los usuarios con el Rol de "Reclutador" puedan ver las notificaciones --}}
-                        @if (auth()->user()->rol === 2)
+                        {{-- Se muestra si el usuario cumple con la lógica del Policy de la ruta: --}}
+                        {{-- Linea: 54 --}}
+                        {{-- Archivo: app\Policies\VacantePolicy.php --}}
+
+                        {{-- @can('[NOMBRE DEL MÉTODO DEL POLICY]', [RUTA DEL MODELO]) => Directiva de Laravel que ejecuta el código dentro solos si el la condición del Policy se cumple.
+                                                                                            Tambien existe la directiva opesta a esta, que es "@cannot()"--}}
+                        {{-- 1er Parametro => Nombre del método del Policy --}}
+                        {{-- 2do Parametro => Ruta del Modelo (De otra forma no funcionará) --}}
+                        @can('create', App\Models\Vacante::class)
                             <a href="{{ route('notificaciones.index') }}" class="mr-2 w-7 bg-indigo-600 hover:bg-indigo-800 rounded-full flex flex-col justify-center items-center text-sm font-extrabold text-white">
                                 {{-- auth()->user()->unreadNotifications->count() => Muestra la cantidad de Notificaciones sin ver --}}
                                 {{ auth()->user()->unreadNotifications->count() }}
                             </a>
-                        @endif
+                        @endcan
 
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
@@ -95,29 +111,45 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         @auth
-            <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
-                    {{ __('Mis Vacantes') }}
-                </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
-                    {{ __('Crear Vacante') }}
-                </x-responsive-nav-link>
+            {{-- Se muestra si el usuario cumple con la lógica del Policy de la ruta: --}}
+            {{-- Linea: 54 --}}
+            {{-- Archivo: app\Policies\VacantePolicy.php --}}
 
-                {{-- Mostramos la Cantidad de Notificaciones sin ver --}}
-                {{-- Validamos que solo los usuarios con el Rol de "Reclutador" puedan ver las notificaciones --}}
-                @if (auth()->user()->rol === 2)
-                    <div class="flex gap-2 items-center p-3 border-t border-gray-200">
-                        <a href="{{ route('notificaciones.index') }}" class="mr-2 w-7 bg-indigo-600 hover:bg-indigo-800 rounded-full flex flex-col justify-center items-center text-sm font-extrabold text-white">
-                            {{-- auth()->user()->unreadNotifications->count() => Muestra la cantidad de Notificaciones sin ver --}}
-                            {{ auth()->user()->unreadNotifications->count() }}
-                        </a>
-                        <p class="text-base font-medium text-gray-600">
-                            {{-- @choice() => Según el valor numero que se le pase como 2do Parametro, este identificará si debe mostrar el String 1 o 2 que se le pase como 1er Parametro  --}}
-                            @choice('Notificacion|Notificaciones', auth()->user()->unreadNotifications->count())
-                        </p>
-                    </div>
-                @endif
-            </div>
+            {{-- @can('[NOMBRE DEL MÉTODO DEL POLICY]', [RUTA DEL MODELO]) => Directiva de Laravel que ejecuta el código dentro solos si el la condición del Policy se cumple.
+                                                                                Tambien existe la directiva opesta a esta, que es "@cannot()"--}}
+            {{-- 1er Parametro => Nombre del método del Policy --}}
+            {{-- 2do Parametro => Ruta del Modelo (De otra forma no funcionará) --}}
+            @can('create', App\Models\Vacante::class)
+                <div class="pt-2 pb-3 space-y-1">
+                    <x-responsive-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
+                        {{ __('Mis Vacantes') }}
+                    </x-responsive-nav-link>
+                    <x-responsive-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
+                        {{ __('Crear Vacante') }}
+                    </x-responsive-nav-link>
+
+                        {{-- Se muestra si el usuario cumple con la lógica del Policy de la ruta: --}}
+                        {{-- Linea: 54 --}}
+                        {{-- Archivo: app\Policies\VacantePolicy.php --}}
+
+                        {{-- @can('[NOMBRE DEL MÉTODO DEL POLICY]', [RUTA DEL MODELO]) => Directiva de Laravel que ejecuta el código dentro solos si el la condición del Policy se cumple.
+                                                                                            Tambien existe la directiva opesta a esta, que es "@cannot()"--}}
+                        {{-- 1er Parametro => Nombre del método del Policy --}}
+                        {{-- 2do Parametro => Ruta del Modelo (De otra forma no funcionará) --}}
+                        @can('create', App\Models\Vacante::class)
+                            <div class="flex gap-2 items-center p-3 border-t border-gray-200">
+                                <a href="{{ route('notificaciones.index') }}" class="mr-2 w-7 bg-indigo-600 hover:bg-indigo-800 rounded-full flex flex-col justify-center items-center text-sm font-extrabold text-white">
+                                    {{-- auth()->user()->unreadNotifications->count() => Muestra la cantidad de Notificaciones sin ver --}}
+                                    {{ auth()->user()->unreadNotifications->count() }}
+                                </a>
+                                <p class="text-base font-medium text-gray-600">
+                                    {{-- @choice() => Según el valor numero que se le pase como 2do Parametro, este identificará si debe mostrar el String 1 o 2 que se le pase como 1er Parametro  --}}
+                                    @choice('Notificacion|Notificaciones', auth()->user()->unreadNotifications->count())
+                                </p>
+                            </div>
+                        @endcan
+                </div>
+            @endcan
 
             <!-- Responsive Settings Options -->
             <div class="pt-4 pb-1 border-t border-gray-200">
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 
 // Para esto agregamos el valor "verified" en la función "middleware([])"
 // Agregamos una 2da validación para que el usuario pueda ver las páginas de la plataforma
-Route::get('/dashboard',[ VacanteController::class, 'index'])->middleware(['auth' ,'verified'])->name('vacantes.index');
+Route::get('/dashboard',[ VacanteController::class, 'index'])->middleware(['auth' ,'verified','rol.reclutador'])->name('vacantes.index');
 Route::get('/vacantes/create',[ VacanteController::class, 'create'])->middleware(['auth' ,'verified'])->name('vacantes.create');
 Route::get('/vacantes/{vacante}/edit',[ VacanteController::class, 'edit'])->middleware(['auth' ,'verified'])->name('vacantes.edit');
 Route::get('/vacantes/{vacante}',[ VacanteController::class, 'show'])->name('vacantes.show');
